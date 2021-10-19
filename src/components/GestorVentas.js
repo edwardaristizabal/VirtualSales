f81068cc826c73d4f3493c0ade15801021290336
Edward Andrés Aristizábal Montalvo <edwardaristizabal@yahoo.es>
2021-10-18 22:36:04 -0500

    Interfaces Terminadas
    Se cargan interfaces terminadas para cumplir con los Sprints 3 y 4, cabe aclarar que el Oauth2 falta una parte que no se logró conectar, pero está tomando los datos de las personas que autorizan el acceso por Facebook y por Google.

--- src/components/GestorVentas.js
+++ src/components/GestorVentas.js
@@ -1,62 +1,95 @@
-import React from 'react'
-import home from '../images/home.png'
-import logo from '../images/logo.png'
+import React from "react";
+import home from "../images/home.png";
+import logo from "../images/logo.png";
 
 function GestorVentas() {
-    return (
-        <div align="center">
-         <div class="text-end">
+  return (
+    <div align="center">
+      <div class="text-end">
         <img src={logo} width="60" alt=""></img>
-        </div>        
-        <h3>Gestor de Ventas</h3>
-        <br/>
-        
-       
-        <div class="btn-group me-2" role="group">       
-        <button type="button" class="btn btn-secondary btn-lg ">Crear</button></div>
-        <div class="btn-group me-2" role="group" >
-        <button type="button" class="btn btn-secondary btn-lg">Editar</button></div>
-        <div class="btn-group me-5" role="group">
-        <button type="button" class="btn btn-secondary btn-lg">Eliminar</button></div>
-        <div class="btn-group me-4" role="group"> 
-        <input type="search" name="Buscar" id="Buscar" placeholder="Buscar" size="50"></input></div>
-        <label>"Usuario"</label>  
-        
-        <br/><br/>
-        
-        <table border="2" class="table table-gray table-striped">
-            <thead align="middle">
-            <th>Codigo de Venta</th>
-            <th>Estado</th>
-            <th>Fecha Estado</th>
-            <th>Fecha Estimada de Entrega</th>
+      </div>
+      <h3>Gestor de Ventas</h3>
+      <br />
 
-            </thead>
-            <tbody>
-            
-            <tr align="middle"><td>No aplica</td>
-                <td>No aplica</td>
-                <td>No aplica</td>
-                <td>No aplica</td>
-            </tr>
-            <tr align="middle">
-                <td>No aplica</td>
-                <td>No aplica</td>
-                <td>No aplica</td>
-                <td>No aplica</td>
-            </tr>
-            </tbody>
+      <div class="btn-group me-2" role="group">
+        <a href="/Crear_Venta">
+          <button type="button" class="btn btn-secondary btn-lg ">
+            Crear
+          </button>
+        </a>
+      </div>
+      <div class="btn-group me-2" role="group">
+        <a href="/Editar_Venta">
+          <button type="button" class="btn btn-secondary btn-lg">
+            Editar
+          </button>
+        </a>
+      </div>
+      <div class="btn-group me-5" role="group">
+        <button type="button" class="btn btn-secondary btn-lg">
+          Eliminar
+        </button>
+      </div>
+      <div class="btn-group me-4" role="group">
+        <input
+          type="search"
+          name="Buscar"
+          id="Buscar"
+          placeholder="Buscar"
+          size="50"
+        ></input>
+      </div>
+      <label>"Usuario"</label>
 
-        </table>
+      <br />
+      <br />
 
-        <div class="btn-group me-2" role="group">   
-        <button type="button" class="btn btn-secondary btn-lg">Reporte</button></div>
-        <div class="btn-group me-5" role="group">
-        <button type="button" class="btn btn-secondary btn-lg">Clientes</button></div>
-        <a href="/Principal"><img src={home} alt="logo" height="50"/></a>
-            
-        </div>
-    )
+      <table border="2" class="table table-gray table-striped">
+        <thead align="middle">
+          <th>Codigo de Venta</th>
+          <th>Estado</th>
+          <th>Fecha Estado</th>
+          <th>Fecha Estimada de Entrega</th>
+        </thead>
+        <tbody>
+          <tr align="middle">
+            <a href="/HistorialVentas">
+              <td>No aplica</td>
+            </a>
+            <td>No aplica</td>
+            <td>No aplica</td>
+            <td>No aplica</td>
+          </tr>
+          <tr align="middle">
+            <a href="/HistorialVentas">
+              <td>No aplica</td>
+            </a>
+            <td>No aplica</td>
+            <td>No aplica</td>
+            <td>No aplica</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div class="btn-group me-2" role="group">
+        <a href="/Reporte_Venta">
+          <button type="button" class="btn btn-secondary btn-lg">
+            Reporte
+          </button>
+        </a>
+      </div>
+      <div class="btn-group me-5" role="group">
+        <a href="/CrearUsuarioCliente">
+          <button type="button" class="btn btn-secondary btn-lg">
+            Clientes
+          </button>
+        </a>
+      </div>
+      <a href="/Principal">
+        <img src={home} alt="logo" height="50" />
+      </a>
+    </div>
+  );
 }
 
-export default GestorVentas
+export default GestorVentas;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,50 +1,95 @@
 import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import AdmVentas from '../components/AdmVentas'
-import Cambiarcontrasena from '../components/Cambiar_contrasena'
-import CrearProducto from '../components/CrearProducto'
-import EditarProducto from '../components/Editar_Producto'
-import GestorVentas from '../components/GestorVentas'
-import HistorialVentas from '../components/HistorialVentas'
-import Login from '../components/Login'
-import Principal from '../components/Principal'
-import Recuperarcontrasena from '../components/Recuperar_contrasena'
+import AdmProductos from "../components/AdmProductos";
+import AdmUsuarios from "../components/AdmUsuarios";
+import AdmVentas from "../components/AdmVentas";
+import Cambiarcontrasena from "../components/Cambiar_contrasena";
+import CrearVenta from "../components/Crear_Venta";
+import CrearAdmUsuarios from "../components/CrearAdmUsuarios";
+import CrearProducto from "../components/CrearProducto";
+import CrearUCliente from "../components/CrearUsuarioCliente";
+import EditarProducto from "../components/Editar_Producto";
+import EditarVenta from "../components/Editar_Venta";
+import EditarAdmUsuario from "../components/EditarAdmUsuario";
+import GestorVentas from "../components/GestorVentas";
+import HistorialCompraCliente from "../components/HistorialCompraCliente";
+import HistorialVentas from "../components/HistorialVentas";
+import Login from "../components/Login";
+import Principal from "../components/Principal";
+import Recuperarcontrasena from "../components/Recuperar_contrasena";
+import ReporteProducto from "../components/Reporte_Productos";
+import ReporteUsuarios from "../components/Reporte_Usuarios";
+import ReporteVenta from "../components/Reporte_Venta";
 
 export default function AppRouter() {
     return (
-        <Router>
-            <Switch>
-            <Route path="/GestordeVentas">
-                <GestorVentas/>
-            </Route>
-            <Route path="/AdministradordeVentas">
-               <AdmVentas/>
-            </Route>
-            <Route path="/Principal">
-               <Principal/>
-            </Route>
-            <Route path="/CambiodeContraseña">
-               <Cambiarcontrasena/>
-            </Route>
-            <Route path="/RecuperarContraseña">
-               <Recuperarcontrasena/>
-            </Route>
-            <Route path="/HistorialVentas">
-               <HistorialVentas/>
-            </Route>
-            <Route path="/CrearProducto">
-               <CrearProducto/>
-               </Route>
-            <Route path="/EditarProducto">
-               <EditarProducto/>
-            </Route>
-            <Route path="/">
-                <Login/>
-            </Route>
-
-            </Switch>
-
-        </Router>
-        )
+      <Router>
+        <Switch>
+          <Route path="/AdmProductos">
+            <AdmProductos />
+          </Route>
+          <Route path="/AdmUsuarios">
+            <AdmUsuarios />
+          </Route>
+          <Route path="/AdmVentas">
+            <AdmVentas />
+          </Route>
+          <Route path="/Cambiar_contrasena">
+            <Cambiarcontrasena />
+          </Route>
+          <Route path="/Crear_Venta">
+            <CrearVenta />
+          </Route>
+          <Route path="/CrearAdmUsuarios">
+            <CrearAdmUsuarios />
+          </Route>
+          <Route path="/CrearProducto">
+            <CrearProducto />
+          </Route>
+          <Route path="/CrearUsuarioCliente">
+            <CrearUCliente />
+          </Route>
+          <Route path="/Editar_Producto">
+            <EditarProducto />
+          </Route>
+          <Route path="/Editar_Venta">
+            <EditarVenta />
+          </Route>
+          <Route path="/EditarAdmUsuario">
+            <EditarAdmUsuario />
+          </Route>
+          <Route path="/GestorVentas">
+            <GestorVentas />
+          </Route>
+          <Route path="/HistorialCompraCliente">
+            <HistorialCompraCliente />
+          </Route>
+          <Route path="/HistorialVentas">
+            <HistorialVentas />
+          </Route>
+          <Route path="/Login">
+            <Login />
+          </Route>
+          <Route path="/Principal">
+            <Principal />
+          </Route>
+          <Route path="/Recuperar_contrasena">
+            <Recuperarcontrasena />
+          </Route>
+          <Route path="/Reporte_Productos">
+            <ReporteProducto />
+          </Route>
+          <Route path="/Reporte_Usuarios">
+            <ReporteUsuarios />
+          </Route>
+          <Route path="/Reporte_Venta">
+            <ReporteVenta />
+          </Route>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    );
 }
 
